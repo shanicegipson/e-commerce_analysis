3584de94724e7c1213b85543efe7d140698599b4
shanice <shanicegipson13@gmail.com>
2025-06-24 15:12:06 -0500

    WIP: Added logic for the top 5 and 10 producs sold. COMPLETED. Next will work on retrieving correlating descriptions for the top selling items and verify accurate total sales.

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,17 +4,42 @@ import test
 file_path = '../data/raw/data.csv'
 
 try:
-    sales_data = pd.read_csv(file_path)
+    sales_df = pd.read_csv(file_path)
 except UnicodeDecodeError:
-    sales_data = pd.read_csv(file_path, encoding="latin-1")  # Or another encoding like "ISO-8859-1"
+    sales_df = pd.read_csv(file_path, encoding="latin-1")  # Or another encoding like "ISO-8859-1"
 
 
 
-print(sales_data.isnull().sum())
+print(sales_df.dtypes)
 
 ####### Calculates the total sales - COMPLETED!
-#quantity = sales_data['Quantity']
-#unit_price = sales_data['UnitPrice']
-#total_sales = quantity * unit_price
-#print(total_sales.sum())
+# quantity = sales_df['Quantity']
+# unit_price = sales_df['UnitPrice']
+# total_sales = quantity * unit_price
+# print(total_sales.sum())
+
+
+####### Grabs the top 5/ 10 items sold - COMPLETED!
+top_five_products = sales_df['StockCode'].str.upper().value_counts()[:5]
+top_ten_products = sales_df['StockCode'].str.upper().value_counts()[:10]
+
+desctiption = sales_df['Description'].value_counts()
+print("================")
+print(stock_code)
+# print(desctiption)
+
+
+print("================")
+# top_sale = sales_df['StockCode'].value_counts()['85123a']
+# print(f"Top item: {top_sale}")
+# for index, row in sales_data.iterrows():
+#     rows_value = row.to_dict()
+#     counts = {}
+#     times_run = 0
+#
+#     if times_run < 3:
+#         for product_code in product_codes:
+#             counts[product_code] = (row == product_code).sum()
+#     print(f"Row {index}: {counts}")
+#     break
 
